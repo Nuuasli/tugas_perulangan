b6c963708753235a9b9265d4bb4a99db9e881cc3
Nuu <126638642+Nuuasli@users.noreply.github.com>
2024-04-13 22:42:36 +0700

    Add files via upload

---
+++ LATIHAN PHP/array.php
@@ -0,0 +1,12 @@
+<?php
+$b = [
+    "nama" => "noven",
+    "pekerjaan" => "programer",
+    "pendidikan" => "S1",
+    "umur" => 7,
+    "nilai" => "d",
+    "hobi" => [
+        "tidur", "duduk", "rebahan"
+    ]
+
+]; 
